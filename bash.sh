fadf29e0fdfcada62f1ff408f9636cf4499322b1
Sam <100165896+miss-sam@users.noreply.github.com>
2022-02-28 14:52:20 -0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,9 +1,19 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 echo Hello $USER to bash. We are going to list the characters from Encanto.
 
 mkdir Encanto
 
-touch Bruno Isabella Lusia Camilo Dolores Mirabel
+touch Abuela Bruno Lusia Camilo Dolores Mirabel
+
+cd Encanto
+
+mkdir GenerationOne GenerationTwo GenerationThree
+
+mv Abuela GenerationOne
+
+mv Bruno GenerationTwo
+
+mv Mirabel GenerationThree
 
 cat Encanto
